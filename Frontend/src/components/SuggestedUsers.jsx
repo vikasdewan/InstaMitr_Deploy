94dd384449dddb073676e531c921bad343da0925
VikasDewangan <dewanganvikas192@gmail.com>
2024-11-30 03:14:25 +0530

    Remove duplicate React import and fix import order in SuggestedUsers component.

--- Frontend/src/components/SuggestedUsers.jsx
+++ Frontend/src/components/SuggestedUsers.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import React, { useEffect, useState } from "react";
+ 
 import {useDispatch,useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
