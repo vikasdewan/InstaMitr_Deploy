dc8adb43d354f841c680dcf5d75a10ca6af95a05
VikasDewangan <dewanganvikas192@gmail.com>
2024-12-03 14:16:11 +0530

    "Added useEffect and useState imports to Home.jsx"

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Feed from "./Feed.jsx";
 import RightSideBar from "./RightSideBar.jsx";
 import { Outlet } from "react-router-dom";
