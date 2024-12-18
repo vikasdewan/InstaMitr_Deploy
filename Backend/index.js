b38eb9ce08c7cc85b2d2248eff652f156873be28
VikasDewangan <dewanganvikas192@gmail.com>
2024-12-18 21:09:34 +0530

    `Update CORS origin to allow multiple domains`

--- Backend/index.js
+++ Backend/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-  origin: process.env.URL,
+  origin: [process.env.URL,"https://instamitr.netlify.app"],
   credentials: true,
 };
 app.use(cors(corsOptions));
