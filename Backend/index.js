933a63c1aa54f7e349911d6befecf7f0bd96971c
VikasDewangan <dewanganvikas192@gmail.com>
2024-12-18 23:39:30 +0530

    `Update origin URLs to https://instamitr-deploy-1.onrender.com in Backend and Frontend code`

--- Backend/index.js
+++ Backend/index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-  origin: process.env.URL,
+  origin:"https://instamitr-deploy-1.onrender.com",
   credentials: true,
 };
 app.use(cors(corsOptions));
--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -8,7 +8,7 @@ const server =  http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:process.env.URL,
+        origin:"https://instamitr-deploy-1.onrender.com",
         methods:['GET' , 'POST']
     }
 })
