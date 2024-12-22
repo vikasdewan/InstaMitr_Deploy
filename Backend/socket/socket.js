74b0afd641b73078f4593a01517c49e5ff430954
VikasDewangan <dewanganvikas192@gmail.com>
2024-12-22 17:26:01 +0530

    `Update socket.js to use new origin in cors configuration`

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -8,7 +8,7 @@ const server =  http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"https://instamitr-deploy-1.onrender.com",
+        origin:"https://instamitr.vercel.app",
         methods:['GET' , 'POST']
     }
 })
