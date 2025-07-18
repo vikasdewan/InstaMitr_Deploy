af568fd0146f7667dc74f6bcba14531d5e30ff18
VikasDewangan <dewanganvikas192@gmail.com>
2025-07-18 16:27:56 +0530

    `Update CommentSection.jsx to use production API URL`

--- Frontend/src/components/common/CommentSection.jsx
+++ Frontend/src/components/common/CommentSection.jsx
@@ -27,7 +27,7 @@ const CommentSection = ({ postId }) => {
     const fetchComments = async () => {
       try {
         const res = await axios.get(
-          `http://localhost:8000/api/v1/post/${postId}/comment/all`,
+          `https://instamitr.onrender.com/api/v1/post/${postId}/comment/all`,
           { withCredentials: true }
         );
         dispatch(setComments(res.data.comments || []));
