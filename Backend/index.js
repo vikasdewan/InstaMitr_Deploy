cb2c3e4a52121074a696e3d87d4da05f32243fab
Vikas Dewangan <138798400+vikasdewan@users.noreply.github.com>
2025-09-23 15:22:20 +0530

    `Fixed typo in import statement for reelRoute`

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
-import reelRoute from "./routes/reel.route.js";
+import reelRoute from "./routes/reels.route.js";
 import { app, server } from "./socket/socket.js";
 import path from "path";
 import passport from "./config/passport.js";
